9def75ad9e1ea10e7740cad190c24377983220cc
fdodino <fernando.dodino@gmail.com>
2016-09-07 15:30:46 -0300

    Removing lines - example wasn't initial at all!

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,20 +1,4 @@
 app.controller('TareasController', function(tareasService) {
 
-	var self = this;
-	
-	this.getTareas = function() {
-	    tareasService.findAll(function(data) {
-	    	self.tareas = _.map(data.data, Tarea.asTarea);
-	    });
-	}	
-
-	this.cumplir = function(tarea) {
-		tarea.cumplir();
-	    tareasService.update(tarea, function() {
-	        self.getTareas();
-	    });
-	}
-	
-	this.getTareas();
 });
 
--- src/main/webapp/js/service.js
+++
@@ -1,9 +0,0 @@
-app.service('tareasService', function($http) {
-    this.findAll = function(callback) {
-        $http.get('/tareas').then(callback);
-    }
-    this.update = function(tarea, callback) {
-        $http.put('/tareas/' + tarea.id, tarea).then(callback);
-   }
-});
-
